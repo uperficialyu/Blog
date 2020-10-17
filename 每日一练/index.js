6c9862a9e3800eec1ebb83411f5607d700ef61d2
emily <947201766@qq.com>
2020-10-17 13:54:07 +0800

    vuetimu

--- 每日一练/index.js
+++ 每日一练/index.js
@@ -1,4 +1,41 @@
-var arr1 = [1, 2, 3, 4];
-var arr2 = arr1;
-arr2[0] = 3;
-console.log(arr2); // [3,2,3,4]
+let ary = [
+	{ name: 1, age: 2, number: 1, son: 'son1' },
+	{ name: 2, age: 23, number: 2, son: 'son2' },
+	{ name: 2, age: 22, number: 3, son: 'son3' },
+	{ name: 1, age: 12, number: 4, son: 'son4' },
+	{ name: 1, age: 42, number: 5, son: 'son5' }
+]
+
+function fn(ary) {
+	let arr = [];
+	ary.forEach(item => {
+		let bol = arr.some(val => {
+			if (val.name === item.name) {
+				let obj = {};
+				Object.keys(item).forEach(v => {
+					if (v != 'name' && v != 'age') {
+						obj[v] = item[v]
+					}
+				})
+				val.list.push(obj);
+				return true
+			}
+		})
+		if (!bol) {
+			let obj = {};
+			Object.keys(item).forEach(v => {
+				if (v != 'name' && v != 'age') {
+					obj[v] = item[v]
+				}
+			})
+			arr.push({ name: item.name, list: [obj] });
+		}
+	})
+	return arr;
+}
+fn(ary)
+console.log(fn(ary))
+
+
+var aa = /^[1-9]\d+(\.\d+)?$/;
+var bb= 0;
