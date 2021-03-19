28c3cf481572e78844d08b7e667507ac0bece5e2
YuEmily <947201766@qq.com>
2021-03-19 20:53:46 +0800

    面试总结

--- index.js
+++ index.js
@@ -1,51 +1,32 @@
-function swap(a, b) {
-  let temp = a;
-  a = b;
-  b = temp;
-}
-
-let arr = [1, 3, 4, 2, 4, 5, 2, 1, 6, 3];
-
-const sort = (arr) => {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+const quickSort = (arr) => {
+  // 数组指定两个位置进行值交换
+  let swap = (arr, i, j) => {
+    let tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
+  }
+  // 完成一次划分交换
+  let findCenter = (arr, left, right) => {
+    let flag = arr[left]
+    let idx = left + 1
+    for (let i = idx; i <= right; i++) {
+      if (arr[i] < flag) {
+        swap(arr, idx, i)
+        idx++
       }
     }
+    swap(arr, left, idx - 1)
+    return idx
   }
-  return arr;
-}
-
-console.log(sort(arr))
-
-function swap(arr, i, j) {
-  let temp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = temp;
-  return arr;
-}
-Array.prototype.bubble = function bubble() {
-  let flag = false;
-  // 外层循环I控制比较的轮数
-  for (let i = 0; i < this.length - 1; i++) {
-    // 里层循环控制每一轮比较的次数J
-    for (let j = 0; j < this.length - 1 - i; j++) {
-      if (this[j] > this[j + 1]) {
-        // 当前项大于后一项，交换位置
-        swap(this, j, j + 1);
-        flag = true;
-      }
+  // 递归排序
+  let sort = (arr, left, right) => {
+    if (left < right) {
+      let center = findCenter(arr, left, right)
+      sort(arr, left, center - 1)
+      sort(arr, center, right)
     }
-    if (!flag) break;
-    flag = false;
   }
-  return this;
+  sort(arr, 0, arr.length - 1)
+  return arr
 }
-let ary = [12, 8, 24, 16, 1];
-ary.bubble();
-console.log(ary);
-
-
+console.log(quickSort([2,4,6,11,22,3,7]))
