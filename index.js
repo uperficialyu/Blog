3c3ae4cebe4986f7b1b51b3b587bf68e94adc6b0
Email <947201766@qq.com>
2021-03-20 20:17:32 +0800

    某建筑公司面试

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
-const quickSort = (arr) => {
-  // 数组指定两个位置进行值交换
-  let swap = (arr, i, j) => {
-    let tmp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = tmp
-  }
-  // 完成一次划分交换
-  let findCenter = (arr, left, right) => {
-    let flag = arr[left]
-    let idx = left + 1
-    for (let i = idx; i <= right; i++) {
-      if (arr[i] < flag) {
-        swap(arr, idx, i)
-        idx++
-      }
-    }
-    swap(arr, left, idx - 1)
-    return idx
-  }
-  // 递归排序
-  let sort = (arr, left, right) => {
-    if (left < right) {
-      let center = findCenter(arr, left, right)
-      sort(arr, left, center - 1)
-      sort(arr, center, right)
-    }
-  }
-  sort(arr, 0, arr.length - 1)
-  return arr
+function A() {
+  this.x = 100;
 }
-console.log(quickSort([2,4,6,11,22,3,7]))
+A.prototype.getX = function getX() {
+  console.log(this.x);
+};
+
+function B() {
+  //CALL继承
+  A.call(this);  //=>this.x = 100;  b.x=100;
+  this.y = 200;
+}
+B.prototype.getY = function getY() {
+  console.log(this.y);
+};
+let b = new B;
+console.log(b);
