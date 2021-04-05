2d0b0b3e569f12a361c6a9a17bd8f36cbe31b747
Email <947201766@qq.com>
2021-04-05 08:25:25 +0800

    数组去重

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-function sumBigNumber(a, b) {
-  var res = '',
-    temp = 0;
-  a = a.split('');
-  b = b.split('');
-  while (a.length || b.length || temp) {
-    temp += ~~a.pop() + ~~b.pop();
-    res = (temp % 10) + res;
-    temp = temp > 9;
-  }
-  return res.replace(/^0+/, '');
-}
+let ary = [12, 23, 12, 15, 25, 23, 25, 14, 16];
+let str = ary.sort((a, b) => a - b).join('@') + '@';
+let reg = /(\d+@)\1*/g;
+ary = [];
+str.replace(reg, (n, m) => {
+  m = Number(m.slice(0, m.length - 1));
+  ary.push(m);
+});
+console.log(ary);
