cf0fb5ac3eea1f88663932ee28d826c075462c19
Email <947201766@qq.com>
2021-04-16 08:27:34 +0800

    数组扁平化

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-let ary = [12, 23, 12, 15, 25, 23, 25, 14, 16];
-let str = ary.sort((a, b) => a - b).join('@') + '@';
-let reg = /(\d+@)\1*/g;
-ary = [];
-str.replace(reg, (n, m) => {
-  m = Number(m.slice(0, m.length - 1));
-  ary.push(m);
-});
-console.log(ary);
+let arr = [
+  [1, 2, 2],
+  [3, 4, 5, 5],
+  [6, 7, 8, 9, [11, 12, [12, 13, [14]]]], 10
+];
+~ function () {
+  function myFlat() {
+    let result = [],
+      _this = this;
+    //=>循环ARR中的每一项，把不是数组的存储到新数组中
+    let fn = (arr) => {
+      for (let i = 0; i < arr.length; i++) {
+        let item = arr[i];
+        if (Array.isArray(item)) {
+          fn(item);
+          continue;
+        }
+        result.push(item);
+      }
+    };
+    fn(_this);
+    return result;
+  }
+  Array.prototype.myFlat = myFlat;
+}();
+let arr1 = arr.myFlat();
+console.log(arr1);
