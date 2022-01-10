28b78ea6b50f19ae57893d0da61e256fbce32418
yushunping <yushunping@app315.net>
2022-01-10 21:31:06 +0800

    上班时间

--- 上班/2022年上班时间.js
+++ 上班/2022年上班时间.js
@@ -5,5 +5,6 @@ const arr = [
   {date: '2022-01-06', startTime: '08:43', endtime: '19.03', week: '周四' },
   {date: '2022-01-07', startTime: '13:17', endtime: '19.22', week: '周五' },
   {date: '2022-01-08', startTime: '10:32', endtime: '20.36', week: '周六假期加班' },
-  {date: '2022-01-09', startTime: '10:24', endtime: '22.01', week: '周日假期加班' },
+  {date: '2022-01-09', startTime: '10:24', endtime: '20.25', week: '周日假期加班' },
+  {date: '2022-01-10', startTime: '08:30', endtime: '', week: '周一' },
 ]
