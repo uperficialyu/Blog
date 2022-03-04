a0e974713e8e2fac77463c33fa908a8a1eaafad5
yushunping <yushunping@app315.net>
2022-03-04 15:14:47 +0800

    文档

--- 上班/2022年上班时间.js
+++ 上班/2022年上班时间.js
@@ -1,4 +1,4 @@
-const arr = [
+const arr1 = [
   {date: '2022-01-03', startTime: '09:53', endtime: '18.08', week: '周一元旦假期加班' },
   {date: '2022-01-04', startTime: '08:37', endtime: '22.17', week: '周二' },
   {date: '2022-01-05', startTime: '09:23', endtime: '19.29', week: '周三' },
@@ -17,11 +17,28 @@ const arr = [
   {date: '2022-01-18', startTime: '11:55', endtime: '22.33', week: '周二' },
   {date: '2022-01-19', startTime: '09:35', endtime: '08.47', week: '周三' },
   {date: '2022-01-20', startTime: '08:39', endtime: '01.19', week: '周四' },
-  {date: '2022-01-21', startTime: '09:38', endtime: '', week: '周五' },
+  {date: '2022-01-21', startTime: '09:38', endtime: '18.06', week: '周五' },
+  {date: '2022-01-25', startTime: '08:20', endtime: '19.47', week: '周二' },
+  {date: '2022-01-26', startTime: '08:33', endtime: '22.02', week: '周三' },
+  {date: '2022-01-27', startTime: '12:10', endtime: '18.38', week: '周四' },
+  {date: '2022-01-28', startTime: '08:24', endtime: '18.01', week: '周五' },
 ]
 
-
-
-
-
-15658125666
+const arr2 = [
+  {date: '2022-02-08', startTime: '07:59', endtime: '19.21', week: '周二' },
+  {date: '2022-02-09', startTime: '08:38', endtime: '19.41', week: '周三' },
+  {date: '2022-02-10', startTime: '09:21', endtime: '22.05', week: '周四' },
+  {date: '2022-02-11', startTime: '09:35', endtime: '19.15', week: '周五' },
+  {date: '2022-02-12', startTime: '10:44', endtime: '22.07', week: '周六假期加班' },
+  {date: '2022-02-13', startTime: '10:15', endtime: '01.21', week: '周日假期加班' },
+  {date: '2022-02-15', startTime: '08:24', endtime: '18.36', week: '周二' },
+  {date: '2022-02-16', startTime: '08:49', endtime: '22.08', week: '周三' },
+  {date: '2022-02-17', startTime: '09:22', endtime: '22.11', week: '周四' },
+  {date: '2022-02-18', startTime: '09:20', endtime: '06.04', week: '周五' },
+  {date: '2022-02-22', startTime: '08:33', endtime: '19.20', week: '周二' },
+  {date: '2022-02-23', startTime: '08:42', endtime: '22.07', week: '周三' },
+  {date: '2022-02-24', startTime: '08:53', endtime: '22.01', week: '周四' },
+  {date: '2022-02-25', startTime: '09:41', endtime: '23.54', week: '周五' },
+  {date: '2022-02-26', startTime: '11:17', endtime: '22.24', week: '周六假期加班' },
+  {date: '2022-02-27', startTime: '11:43', endtime: '22.24', week: '周日假期加班' },
+]
