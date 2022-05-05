5149543cdaa9a31c5d623bda676f8437b17d0244
Email <947201766@qq.com>
2022-05-05 19:02:53 +0800

    文档

--- 上班/2022年上班时间.js
+++ 上班/2022年上班时间.js
@@ -42,3 +42,46 @@ const arr2 = [
   {date: '2022-02-26', startTime: '11:17', endtime: '22.24', week: '周六假期加班' },
   {date: '2022-02-27', startTime: '11:43', endtime: '22.24', week: '周日假期加班' },
 ]
+
+const arr4 = [
+  {date: '2022-04-01', startTime: '08:38', endtime: '18.16', week: '周五' },
+  {date: '2022-04-02', startTime: '08:27', endtime: '19.14', week: '周六假期补班' },
+  {date: '2022-04-05', startTime: '08:27', endtime: '21.45', week: '清明加班' },
+  {date: '2022-04-06', startTime: '08:41', endtime: '22.03', week: '周三' },
+  {date: '2022-04-07', startTime: '09:43', endtime: '22.06', week: '周四' },
+  {date: '2022-04-08', startTime: '09:51', endtime: '22.03', week: '周五' },
+  {date: '2022-04-09', startTime: '09:55', endtime: '22.00', week: '周六假期加班' },
+  {date: '2022-04-11', startTime: '13:08', endtime: '19.00', week: '周一' },
+  {date: '2022-04-12', startTime: '13:02', endtime: '22.58', week: '周二' },
+  {date: '2022-04-13', startTime: '09:34', endtime: '22.00', week: '周三' },
+  {date: '2022-04-14', startTime: '09:35', endtime: '22.25', week: '周四' },
+  {date: '2022-04-15', startTime: '09:48', endtime: '21.30', week: '周五' },
+  {date: '2022-04-18', startTime: '08:48', endtime: '21.35', week: '周一' },
+  {date: '2022-04-19', startTime: '13:17', endtime: '22.01', week: '周二' },
+  {date: '2022-04-20', startTime: '09:39', endtime: '22.02', week: '周三' },
+  {date: '2022-04-21', startTime: '09:46', endtime: '19.12', week: '周四' },
+  {date: '2022-04-22', startTime: '06:25', endtime: '18.04', week: '周五' },
+  {date: '2022-04-23', startTime: '10:48', endtime: '18.59', week: '周六假期加班' },
+  {date: '2022-04-24', startTime: '08:44', endtime: '22.16', week: '周日假期补班' },
+  {date: '2022-04-25', startTime: '09:29', endtime: '22.06', week: '周一' },
+  {date: '2022-04-26', startTime: '09:41', endtime: '22.16', week: '周二' },
+  {date: '2022-04-27', startTime: '09:41', endtime: '18.15', week: '周三' },
+  {date: '2022-04-28', startTime: '07:36', endtime: '22.07', week: '周四' },
+]
+
+function workOvertime(arr) {
+  let sum = 0;
+  let sum1 = 0;
+  arr.forEach(item => {
+    if(item.endtime.includes('22.')) {
+      sum++
+    }
+    if(item.week.includes('加班')) {
+      sum1++
+    }
+  })
+  console.log(`22点以后下班的天数为${sum}天`);
+  console.log(`周末加班的天数为${sum1}天`);
+}
+
+workOvertime(arr4);
